cd2d1bc80964a8f9b8186cb0ecfebeb25d607332
Alexander Greff <alexander@greffsite.com>
2024-01-07 16:55:33 -0600

    myvector created, elements added, index iteration

--- cpp/STLPractice/main.cpp
+++ cpp/STLPractice/main.cpp
@@ -7,6 +7,15 @@
 
 int main(int argc, char const *argv[])
 {
-    std::vector
+    std::vector<int> myvector;
+    myvector.push_back(13368);
+    myvector.push_back(18854);
+    myvector.push_back(222345);
+    for (int i = 0; i < myvector.size(); i++)
+    {
+        std::cout <<"myvector[" << i << "]=" << myvector[i] << std::endl;
+        
+    }
+    
     return 0;
 }
